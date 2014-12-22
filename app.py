0e741801ccab01b8adfffde1bb48b9f5a8a12979
Jaren Glover <jaren.g@gmail.com>
2014-12-21 19:21:11 -0500

    updated function doc strings

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello():
 
     """
     try:
-        root_folder_files = get_folder_files()   #gets root folder files
+        root_folder_files = get_folder_files()   #gets root folder files/items
     except Exception as ex:
         return 'There was a problem using the Box Content API: {}'.format(ex.message), 500
 
@@ -35,88 +35,81 @@ def view(file_id):
     """
 
     try:
-        boxcloud_link = get_boxcloud_for_file(file_id)
+        boxcloud_link = get_boxcloud_for_file(file_id)  # obtains the URL for the file from box folder
     except Exception as ex:
         return 'There was a problem using the Box Content API: {}'.format(ex.message), 500
 
-    headers = {
+    headers = {                             # build Box's authorization header
         'Authorization': 'Token {}'.format(s.VIEW_API_KEY),
         'Content-Type': 'application/json',
     }
 
     documents_resource = '/documents'
     url = s.VIEW_API_URL + documents_resource
-    data = json.dumps({'url': boxcloud_link})
-
-    api_response = requests.post(url, headers=headers, data=data)
-    document_id = api_response.json()['id']
+    data = json.dumps({'url': boxcloud_link})     # stores the URL for a POST (upload) request to view api
+    api_response = requests.post(url, headers=headers, data=data) # sends the POST request
+    document_id = api_response.json()['id']         # obtains the id for the document that was uploaded to the view api
     
-    for i in range(30):
-        document_resource = '{}/{}'.format(documents_resource, document_id)
-        url = s.VIEW_API_URL + document_resource
-        api_response = requests.get(url, headers=headers)
-        #print api_response.json()
-        status = api_response.json()['status']
+    for i in range(30):      # for loop to give the files 30 secs to process
+        document_resource = '{}/{}'.format(documents_resource, document_id)   # build the resource with doc id
+        url = s.VIEW_API_URL + document_resource                              # built URL
+        api_response = requests.get(url, headers=headers)                     # GET request for the doc
+
+        status = api_response.json()['status']    # waiting for the document processing to finish
         if status == 'done':
             break
         elif status == 'error':
             break
         else:
-            time.sleep(1)
+            time.sleep(1)       # waits up to 30 secs then failed
 
     if status != 'done':
         return 'There was a problem generating a preview for this document! The error message provide by the api is "{}"'.format(api_response.json()['error_message']), 500
 
-    sessions_resource = '/sessions'
+    sessions_resource = '/sessions'   # To view a document first create a session.
     url = s.VIEW_API_URL + sessions_resource
-    data = json.dumps({'document_id': document_id})
+    data = json.dumps({'document_id': document_id})  # adds doc id for the POST request
 
-    api_response = requests.post(url, headers=headers, data=data)
-    session_id = api_response.json()['id']
+    api_response = requests.post(url, headers=headers, data=data)   # POST request with the doc id
+    session_id = api_response.json()['id']              # obtains session iD
 
-    # TODO(seanrose): make the view base url in settings.py usable here
-    #view_base_url = 'https://view-api.box.com/view'
-    view_url = '{}/{}/view?theme=dark'.format(s.SESSIONS_URL, session_id)
-    #print view_url
+    view_url = '{}/{}/view?theme=dark'.format(s.SESSIONS_URL, session_id)    # obtains the URL with session id w/ theme
+
     return redirect(view_url)
 
 
 def get_folder_items(folder_id=0):
-    """A simple wrapper around the Box View API
+    """ A function that returns a list of items in the folder_id provided
 
     Args:
-        api_token: A valid box view api token, get one here: bit.ly/boxapikey
-
-    Attributes:
+        folder_id: A valid folder's ID - default value is 0
 
     """
-    folders_resource = '/folders/{}/items'.format(folder_id)
-    url = s.CONTENT_API_URL + folders_resource
-    auth = {'Authorization': 'Bearer {}'.format(s.CONTENT_ACCESS_TOKEN)}
+    folders_resource = '/folders/{}/items'.format(folder_id)  # build the resource
+    url = s.CONTENT_API_URL + folders_resource                # build the API's URL
+    auth = {'Authorization': 'Bearer {}'.format(s.CONTENT_ACCESS_TOKEN)} # build Box's authorization header
 
-    api_response = requests.get(url, headers=auth)
-    api_response.raise_for_status()
+    api_response = requests.get(url, headers=auth)    # make request
+    api_response.raise_for_status()                   # check for status
 
-    return api_response.json()
+    return api_response.json()          # JSON full of items details
 
 
 def get_folder_files(folder_id=0):
-    """A simple wrapper around the Box View API
+    """A function that will return a folder and its items
 
     Args:
-        api_token: A valid box view api token, get one here: bit.ly/boxapikey
-
-    Attributes:
+        folder_id: A valid folder's ID - default value is 0
 
     """
-    folder_items = get_folder_items(folder_id)
+    folder_items = get_folder_items(folder_id)    # obtain items in the folder_id
 
     folder_files = [
-        item for item in folder_items['entries'] if item['type'] == 'file'
+        item for item in folder_items['entries'] if item['type'] == 'file'   # loop to obtain only items of type file
     ]
 
-    folder_items['entries'] = folder_files
-    folder_items['total_count'] = len(folder_files)
+    folder_items['entries'] = folder_files              # store files in this sub array
+    folder_items['total_count'] = len(folder_files)    # store length in this sub array
 
     return folder_items
 
@@ -128,16 +121,16 @@ def get_boxcloud_for_file(file_id):
         file_id: Box’s unique string identifying a file.
     """
 
-    files_resource = '/files/{}/content'.format(file_id) #builds the UR: w/ file_id
+    files_resource = '/files/{}/content'.format(file_id) # builds the resource
     url = s.CONTENT_API_URL + files_resource             # build the final URL with API's path
     auth = {'Authorization': 'Bearer {}'.format(s.CONTENT_ACCESS_TOKEN)}
 
-    api_response = requests.get(url, headers=auth, allow_redirects=False) #makes requests
-    api_response.raise_for_status()                                        #check for errors
+    api_response = requests.get(url, headers=auth, allow_redirects=False) # makes request
+    api_response.raise_for_status()                                        # check for errors
 
-    boxcloud_link = api_response.headers['Location']      #the response header is where the location URL is found
+    boxcloud_link = api_response.headers['Location']      # the response header is where the location URL is found
 
-    return boxcloud_link
+    return boxcloud_link      # returns the URL to the file
 
 
 if __name__ == '__main__':
